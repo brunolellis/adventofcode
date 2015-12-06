88d21bd49aeae818af8d156727f11bda56b6d82b
Bruno Lellis <brunolellis@gmail.com>
2015-12-06 16:21:40 -0200

    day 5 - part 2 using regex

--- day5.js
+++ day5.js
@@ -30,8 +30,27 @@ class SantaString {
 		// 3. does NOT contain 'ab', 'cd', 'pq', or 'xy'.
 		return this._countVowels() && this._containsTwoLettersInARow() && this._containsSequence();
 	}
+
+	_containsTwoLettersWithoutOverlapping() {
+		// /(..).*\1/
+		return (this.text.match(/([a-z]{2}).*\1/) || []).length > 0;
+	}
+	_containsTwoSameLettersWithAnotherBetween() {
+		// /(.).\1/
+		return (this.text.match(/([a-z]{1}).{1}\1/) || []).length > 0;
+	}
+
+	isNicePart2() {
+		// 1. pair of 2 letter without overlapping; like 'aabcdefgaa'
+		// 2. at least one letter which repeats with exactly one letter between them.
+		return this._containsTwoLettersWithoutOverlapping() && this._containsTwoSameLettersWithAnotherBetween();
+	}
 }
 
-console.log(strings.split('\n').reduce(function(agg, element) {
+console.log('part 1: ' + strings.split('\n').reduce(function(agg, element) {
 	return new SantaString(element).isNice() ? ++agg : agg;
+}, 0));
+
+console.log('part 2: ' + strings.split('\n').reduce(function(agg, element) {
+	return new SantaString(element).isNicePart2() ? ++agg : agg;
 }, 0));
